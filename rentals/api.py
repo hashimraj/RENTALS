201aa94c2dcede26e05430aa4f982ff65db8bd16
hashimraj <hashimraj02@gmail.com>
2026-04-03 15:50:54 +0300

    doc_events hook

--- rentals/api.py
+++ rentals/api.py
@@ -3,3 +3,6 @@ import frappe
 @frappe.whitelist()
 def get_emoji():
     return "💰"
+
+def throw_emoji(doc, event):
+    frappe.throw("🥲")
--- rentals/hooks.py
+++ rentals/hooks.py
@@ -132,13 +132,11 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+doc_events = {
+	"ToDo": {
+		"before_insert": "rentals.api.throw_emoji"
+	}
+}
 
 # Scheduled Tasks
 # ---------------
